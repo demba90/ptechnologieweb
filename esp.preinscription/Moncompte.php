7d439e99b0cf6958bc7847b52257bb1d1bcf76f5
demba90 <ngagnethiam@esp.sn>
2015-08-01 02:05:20 +0000

    Initialisation
    Je crée les sous documents

---
+++ esp.preinscription/Moncompte.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiam
+ * Date: 31/07/2015
+ * Time: 18:28
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="eLearning is a modern and fully responsive Template by WebThemez.">
+    <meta name="author" content="webThemez.com">
+    <title>ESP Pré-inscription </title>
+    <link rel="favicon" href="assets/images/favicon.png">
+    <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <!-- Custom styles for our template -->
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="assets/css/isotope.css" media="screen" />
+    <link rel="stylesheet" href="assets/js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<!-- Fixed navbar -->
+<div class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <!-- Button for smallest screens -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            <a class="navbar-brand" href="index.html">
+                <img src="assets/images/logo.png" alt="Techro HTML5 template"></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav pull-right mainNav">
+                <li ><a href="index.html">Accueil</a></li>
+                <li><a href="offreformation.html">Offres de formation</a></li>
+                <!--<li><a href="cours.html">Cours</a></li>
+                <li><a href="#">Forum</a></li>
+                <li ><a href="#">About</a></li>-->
+                <!--<li class="active"><a href="#">S'inscrire</a></li><!--
+                    <li><a href="connectre.html">Se Connecter</a></li>
+                    <li><a href="#">Contact</a></li>-->
+            </ul>
+        </div>
+        <!--/.nav-collapse -->
+    </div>
+</div>
+<!-- /.navbar -->
+
+<header id="head" class="secondary">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>Mon Dossier</h1>
+            </div>
+        </div>
+
+    </div>
+</header>
+<!-- container -->
+<section class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <section id="mondossier" class="page-section section appear clearfix">
+                <br />
+                <br />
+                <p class="alert-info" align="center" id="annonce">
+                    <hr width="100%">
+                    Cette page est en conctruction
+                    </br>
+
+                </p>
+
+
+
+            </section>
+
+        </div>
+    </div>
+
+</section>
+<!-- /container -->
---
+++ esp.preinscription/Redirection.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiam
+ * Date: 31/07/2015
+ * Time: 18:28
+ */
+/**
+ * C'est dans cette page où sera redirigé les tantative de connexion
+ * Si on essaye de se connecter, on passe par ici
+ * on vérifie l'authenticité et décider de la redirection vers la page
+ */
+
+    if(isset($_POST["close"])){
+        header('Location: index.html');
+    }elseif(isset($_POST["connexion"])){
+            /*
+             * On vérifie s'il est bien connu par la plateforme et si oui
+             * 1- on enregistre ses information sur une session
+             * 2- on le redirige dans une page ou on affichera à gauche de en haut ses informations personnelles
+             * 3- on lui presentera son dossier
+             * Donc, cette page doit etre une page php
+             * qui est partagée par tous candidats mais avec les sessions
+             * on affichera des informations dynamique
+             * on fera une recherche aussi dynamique pour présenter automatiquement le dossier au candidat
+             *
+             */
+        header('Location: Moncompte.php');
+    }
+?>
